8a3527399fc9e0f5aaffed1d6a8b1fd5ce128fae
thotsl4yer69 <justmyeverything@proton.me>
2025-08-04 19:39:45 +1000

    Add Manus landing page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ManusSite from './ManusSite.jsx'
+
+const path = window.location.pathname
+const RootComponent = path.startsWith('/manus') ? ManusSite : App
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RootComponent />
   </React.StrictMode>
 )
